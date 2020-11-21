0660bb491c8c2ada1ad3c93d198391f354761c31
dedykaryadigunawan <dedykaryadigunawan@gmail.com>
2020-11-21 18:30:28 +0700

    tugas pemograman mobile dedy

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,93 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+import simple.example.hewanpedia.model.Ayam;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+
+    private static List<Ayam> initDataAyam(Context ctx) {
+        List<Ayam> ayams = new ArrayList<>();
+        ayams.add(new Ayam("Plymouth Rock", "Amerika",
+                "Plymouth Rock adalah jenis ayam domestik Amerika. Ini pertama kali terlihat di Massachusetts pada abad kesembilan belas", R.drawable.plymout));
+        ayams.add(new Ayam("Leghorn", "Italia",
+                "The Leghorn adalah jenis ayam yang berasal dari Tuscany, Italia tengah.", R.drawable.leghorn));
+        ayams.add(new Ayam("Australorp", "Australia",
+                "Australorp adalah jenis ayam yang berasal dari Australia, yang dikembangkan sebagai jenis utilitas dengan fokus pada bertelur", R.drawable.australorp));
+        ayams.add(new Ayam("Cochin", "Tiongkok",
+                "Cochin adalah jenis ayam peliharaan besar. Itu berasal dari ayam besar bulu berkaki dibawa dari Cina ke Eropa dan Amerika Utara pada 1840-an dan 1850-an.", R.drawable.cochin));
+        ayams.add(new Ayam("Shamo", "Jepang",
+                "Shamo adalah sebutan keseluruhan untuk gamefowl di Jepang. Ada tujuh ras ayam Shamo yang diakui di Jepang.", R.drawable.shamo));
+        ayams.add(new Ayam("aseel", "Sindh, Punjab",
+                "Asil atau Aseel adalah jenis ayam yang berasal dari anak benua India.", R.drawable.aseel));
+        return ayams;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(initDataAyam(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
